151ffc0a1e120c58255ea9277a34e9865d16bde2
Áron Széles <szelesaron39@gmail.com>
2022-10-30 11:18:09 +0000

    q1 done

---
+++ cw.r
@@ -0,0 +1,36 @@
+#read in file
+data = read.csv("C:/Users/Áron/Google Drive/UNI/Y4S1/Statistical Modelling and Analysis/CW/filesize.csv")
+
+##########QUESTION 1##########
+#plots
+hist(data$x, freq = FALSE, breaks = 1000, 
+     main = "Distribition of values (whole range)  with break = 1000", xlab = "File size (kB)")
+
+hist(data$x, freq = FALSE, breaks = 1000, xlim=c(1000,10000), 
+     main = "Distribition of values (range 1000- 20000) with break = 1000", xlab = "File size (kB)")
+
+boxplot(data, outline = FALSE, col = "YELLOW", xlab = "File size (kB)", horizontal = TRUE,
+        main = "Boxplot of values - outliers filtered", notch = TRUE)
+
+boxplot(data, outline = TRUE, col = "YELLOW", xlab = "File size (kB)", horizontal = TRUE,
+        main = "Boxplot of values - outliers not filtered", notch = TRUE)
+
+#basic data summary - min, 1st q, median, mean, 3rd q, max, std
+summary(data$x)
+sprintf("Standard deviation: %f",sapply(data, sd))
+
+
+
+##########QUESTION 2##########
+
+
+
+
+
+
+
+
+
+
+
+
