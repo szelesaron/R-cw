eb4d67e38335fa1182b5f0cb538d73f728c5e207
Áron Széles <szelesaron39@gmail.com>
2022-11-22 17:26:50 +0000

    q5 done, 6 might be done

--- cw.r
+++ cw.r
@@ -1,3 +1,4 @@
+library(Pareto)
 #read in file
 data = read.csv("C:/Users/Áron/Google Drive/UNI/Y4S1/Statistical Modelling and Analysis/CW/filesize.csv")
 
@@ -17,24 +18,49 @@ boxplot(data, outline = TRUE, col = "YELLOW", xlab = "File size (kB)", horizonta
 
 #basic data summary - min, 1st q, median, mean, 3rd q, max, std
 summary(data$x)
-sprintf("Standard deviation: %f",sapply(data, sd))
+sprintf("Standard deviation: %f",sd(data$x))
+
+
 
 
 
 ##########QUESTION 5##########
-library(Pareto)
+#pareto_sim <- rPareto(1000, 1000, 2.7931)
+#hist(pareto_sim, breaks = 500, main = "Simulated Pareto distribution")
 
-pareto <- dPareto(data$x, 1000 ,2.7931)
-hist(pareto)
 
-for (i in 1:1000) {
-  pareto <- dPareto(data$x, 1000 ,2.7931)
-  hist(pareto)
+#run it 1000 times and record 
+y_prime = c()
+for (i in 1:10000) {
+  x_i_prime <- rPareto(1000, 1000, 2.7931)
+  y_prime <- append(y_prime, mean(x_i_prime))
 }
 
-mean(pareto)
-sum(log(data$x))
+#plot hist
+x <- y_prime
+hist(x, freq = FALSE, breaks = 100, main = "Histogram of distribution of Y' with fitted curve.") 
+curve(dnorm(x, mean = mean(y_prime), sd = sd(y_prime)), add = TRUE, col = "blue", lwd = 2)
 
+#get statistics
+summary(y_prime)
+sprintf("Standard deviation: %f",sd(y_prime))
+
+
+
+##########QUESTION 6##########
+#cdf <- pPareto(1700, 1000, 2.793)
+upper_interval_bound = -1
+
+for (i in 1:5000) {
+  cdf <- pPareto(1000+i, 1000, 2.793)
+  if (cdf >= 0.99)
+  {
+    upper_interval_bound = 1000+i
+    break
+  }
+} 
+sprintf("Upper interval bound: %d",upper_interval_bound)
+sprintf("Confidence interval: %f",pPareto(upper_interval_bound, 1000, 2.793))
 
 
 
